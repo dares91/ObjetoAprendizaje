d3922cac99b1c4aeb55038064a3a0661eddd8359
dares91 <dares1891@gmail.com>
2013-07-22 19:34:28 -0600

    Terminado medidas y formas de propagacion malware

--- js/globalJS.js
+++ js/globalJS.js
@@ -1,3 +1,4 @@
+var clicked=false;
 $(document).ready(function (){
    
     $(document).on("scroll",function (){
@@ -5,7 +6,8 @@ $(document).ready(function (){
             if(pscroll>150)
                $(".staticSideMenu").css({"top":"40px"});   
              else
-                 $(".staticSideMenu").css({"top":(220-pscroll)});        
+                 $(".staticSideMenu").css({"top":(220-pscroll)}); 
+             validarCurrentAnchor(pscroll);
     });
     
     $(".itemStacticMenu ").click(function (){
@@ -19,4 +21,21 @@ $(document).ready(function (){
 function scrollToAnchor(aid){
     var aTag = $("a[name='"+ aid +"']");
     $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+}
+
+function  scrollToTop(){
+     $('html,body').animate({scrollTop: 0},'slow');
+}
+
+ var anchorTop;
+ var anchors=document.anchors;
+function validarCurrentAnchor(scrollPosition){
+    for(var a in anchors){ 
+        anchorTop=$(anchors[a]).offset().top;
+        anchorName= $(anchors[a]).attr("name");
+        if(anchorTop<(scrollPosition+60)){
+            $(".itemStacticMenu").removeClass("activeLink");
+            $("#"+anchorName).addClass("activeLink");
+        }
+    }
 }
